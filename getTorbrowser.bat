d13ec26e89b3334dddb4db09a4520966a42bf6f6
miles guo <komg@hotmail.com>
2022-08-07 21:53:01 +0800

    Add some file to download-list

--- getTorbrowser.bat
+++ getTorbrowser.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 cls
 echo :::::::::::::::::::::::::::::
 echo :: getTorbrowser           ::
-echo :: 2022.07.19 by Miles Guo ::
+echo :: 2022.08.07 by Miles Guo ::
 echo :: Use at your own risk.   ::
 echo :::::::::::::::::::::::::::::
 echo .
@@ -31,38 +31,38 @@ if %PROCESSOR_ARCHITECTURE%==x86 (
 :: https://www.torproject.org/download/
 :: https://dist.torproject.org/torbrowser/11.0.15/
 set test_url=https://www.torproject.org/download/
-set download_url=https://dist.torproject.org/torbrowser/
+set dist_url=https://dist.torproject.org/torbrowser/
 ::
 :: Mirror site. Good choice, but need to run Tor+Privoxy
 :: https://tor.eff.org/download/
 :: https://tor.eff.org/dist/torbrowser/11.0.15/
 ::set test_url=https://tor.eff.org/download/
-::set download_url=https://tor.eff.org/dist/torbrowser/
+::set dist_url=https://tor.eff.org/dist/torbrowser/
 ::
 :: Mirror site. Good choice, no need to run Tor+Privoxy (Recommended for use in China)
 :: https://tor.calyxinstitute.org/download/
 :: https://tor.calyxinstitute.org/dist/torbrowser/11.0.15/
 ::set test_url=https://tor.calyxinstitute.org/download/
-::set download_url=https://tor.calyxinstitute.org/dist/torbrowser/
+::set dist_url=https://tor.calyxinstitute.org/dist/torbrowser/
 ::
 :: Mirror site. Alternative, no need to run Tor+Privoxy (Attention! Not the latest version)
 :: https://mirror.oldsql.cc/tor/download/
 :: https://mirror.oldsql.cc/tor/dist/torbrowser/11.0.14/
 ::set test_url=https://mirror.oldsql.cc/tor/download/
-::set download_url=https://mirror.oldsql.cc/tor/dist/torbrowser/
+::set dist_url=https://mirror.oldsql.cc/tor/dist/torbrowser/
 ::
-echo The current torbrowser download-url is: %download_url%
+echo The current torbrowser dist-url is: %dist_url%
 echo .
 echo We will test whether it is available.
 echo .
 ::
-:: test test-url
+:: test dist-url
 ::
-echo test %test_url%...
+echo test %dist_url%...
 echo .
-%wget% -t 3 --spider %test_url% 
+%wget% -t 3 --spider %dist_url% 
 if %errorlevel%==0 (
-  echo Okay,the %download_url% is available.
+  echo Okay,the %dist_url% is available.
   echo .
   set toraloneprivoxy=n
   goto :readygo
@@ -103,27 +103,29 @@ set https_proxy=127.0.0.1:8118
 echo Okey,Tor+Privoxy has started.
 echo .
 ::
-:: test test-url again until it's available.
+:: test dist-url again until it's available.
 ::
-echo test %test_url% again until it's available...
+echo test %dist_url% again until it's available...
 echo .
 :repeat
-%wget% -t 1 --spider %test_url% 
+%wget% -t 1 --spider %dist_url% 
 if %errorlevel% neq 0 goto :repeat
 ::
-echo Okey,now the %download_url% is available.
+echo Okey,now the %dist_url% is available.
 echo .
 ::
 :readygo
 echo Let's Go.
 echo .
 ::
-set TBpath=
-for /f "tokens=*" %%a in ('%curl% -s %test_url% ^| %grep% -o -E -m 1 "dist/torbrowser/([^/]+)/"') do set TBpath=%%a
-set TBver=%TBpath:~16,-1%
+for /f "delims=" %%a in ('%curl% -s %test_url% ^| %grep% -o -E -m 1 "dist/torbrowser/([^/]+)/"') do set temp=%%a
+set TBver=%temp:~16,-1%
 ::
-for /f "delims=" %%b in ('%curl% -s %download_url%%TBver%/ ^| %grep% -o -E -m 1 ">tor-win64-([^/]+)zip"') do set TBpath1=%%b
-set Tver=%TBpath1:~11,-4%
+for /f "delims=" %%b in ('%curl% -s %dist_url%%TBver%/ ^| %grep% -o -E -m 1 ">tor-win64-([^/]+)zip"') do set temp=%%b
+set Tver=%temp:~11,-4%
+::
+for /f "delims=" %%c in ('%curl% -s %dist_url%%TBver%/ ^| %grep% -o -E -m 1 ">src-tor-launcher-([^/]+)tar.xz"') do set temp=%%c
+set TLver=%temp:~18,-7%
 ::
 set dst=%~dp0Torbrowser\%TBver%
 set opt=-c -t 0 -P %dst%
@@ -133,40 +135,54 @@ set locale=zh-CN
 ::
 :: get torbrowser from download url... 
 ::
-echo get torbrowser from %download_url%...
+echo get torbrowser from %dist_url%...
 echo .
 ::
-%wget% %opt% %download_url%%TBver%/sha256sums-signed-build.txt
-%wget% %opt% %download_url%%TBver%/sha256sums-signed-build.txt.asc
+%wget% %opt% %dist_url%%TBver%/sha256sums-signed-build.txt
+%wget% %opt% %dist_url%%TBver%/sha256sums-signed-build.txt.asc
 ::
-%wget% %opt% %download_url%%TBver%/tor-win32-%Tver%.zip
-%wget% %opt% %download_url%%TBver%/tor-win32-%Tver%.zip.asc
-%wget% %opt% %download_url%%TBver%/tor-win64-%Tver%.zip
-%wget% %opt% %download_url%%TBver%/tor-win64-%Tver%.zip.asc
-%wget% %opt% %download_url%%TBver%/tor-linux32-debug.tar.xz
-%wget% %opt% %download_url%%TBver%/tor-linux32-debug.tar.xz.asc
-%wget% %opt% %download_url%%TBver%/tor-linux64-debug.tar.xz
-%wget% %opt% %download_url%%TBver%/tor-linux64-debug.tar.xz.asc
+%wget% %opt% %dist_url%%TBver%/TorBrowser-%TBver%-osx64_%locale%.dmg
+%wget% %opt% %dist_url%%TBver%/TorBrowser-%TBver%-osx64_%locale%.dmg.asc
 ::
-%wget% %opt% %download_url%%TBver%/torbrowser-install-%TBver%_%locale%.exe
-%wget% %opt% %download_url%%TBver%/torbrowser-install-%TBver%_%locale%.exe.asc
-%wget% %opt% %download_url%%TBver%/torbrowser-install-win64-%TBver%_%locale%.exe
-%wget% %opt% %download_url%%TBver%/torbrowser-install-win64-%TBver%_%locale%.exe.asc
+%wget% %opt% %dist_url%%TBver%/mar-tools-linux32.zip
+%wget% %opt% %dist_url%%TBver%/mar-tools-linux32.zip.asc
+%wget% %opt% %dist_url%%TBver%/mar-tools-linux64.zip
+%wget% %opt% %dist_url%%TBver%/mar-tools-linux64.zip.asc
+%wget% %opt% %dist_url%%TBver%/mar-tools-mac64.zip
+%wget% %opt% %dist_url%%TBver%/mar-tools-mac64.zip.asc
+%wget% %opt% %dist_url%%TBver%/mar-tools-win32.zip
+%wget% %opt% %dist_url%%TBver%/mar-tools-win32.zip.asc
+%wget% %opt% %dist_url%%TBver%/mar-tools-win64.zip
+%wget% %opt% %dist_url%%TBver%/mar-tools-win64.zip.asc
 ::
-%wget% %opt% %download_url%%TBver%/TorBrowser-%TBver%-osx64_%locale%.dmg
-%wget% %opt% %download_url%%TBver%/TorBrowser-%TBver%-osx64_%locale%.dmg.asc
+%wget% %opt% %dist_url%%TBver%/src-tor-launcher-%TLver%.tar.xz
+%wget% %opt% %dist_url%%TBver%/src-tor-launcher-%TLver%.tar.xz.asc
 ::
-%wget% %opt% %download_url%%TBver%/tor-browser-linux32-%TBver%_%locale%.tar.xz
-%wget% %opt% %download_url%%TBver%/tor-browser-linux32-%TBver%_%locale%.tar.xz.asc
+%wget% %opt% %dist_url%%TBver%/tor-browser-%TBver%-android-armv7-multi-qa.apk
+%wget% %opt% %dist_url%%TBver%/tor-browser-%TBver%-android-armv7-multi-qa.apk.asc
+%wget% %opt% %dist_url%%TBver%/tor-browser-%TBver%-android-armv7-multi-qa.apk.idsig
+%wget% %opt% %dist_url%%TBver%/tor-browser-%TBver%-android-armv7-multi.apk
+%wget% %opt% %dist_url%%TBver%/tor-browser-%TBver%-android-armv7-multi.apk.asc
 ::
-%wget% %opt% %download_url%%TBver%/tor-browser-linux64-%TBver%_%locale%.tar.xz
-%wget% %opt% %download_url%%TBver%/tor-browser-linux64-%TBver%_%locale%.tar.xz.asc
+%wget% %opt% %dist_url%%TBver%/tor-browser-linux32-%TBver%_%locale%.tar.xz
+%wget% %opt% %dist_url%%TBver%/tor-browser-linux32-%TBver%_%locale%.tar.xz.asc
+%wget% %opt% %dist_url%%TBver%/tor-browser-linux64-%TBver%_%locale%.tar.xz
+%wget% %opt% %dist_url%%TBver%/tor-browser-linux64-%TBver%_%locale%.tar.xz.asc
 ::
-%wget% %opt% %download_url%%TBver%/tor-browser-%TBver%-android-armv7-multi-qa.apk
-%wget% %opt% %download_url%%TBver%/tor-browser-%TBver%-android-armv7-multi-qa.apk.asc
-%wget% %opt% %download_url%%TBver%/tor-browser-%TBver%-android-armv7-multi-qa.apk.idsig
-%wget% %opt% %download_url%%TBver%/tor-browser-%TBver%-android-armv7-multi.apk
-%wget% %opt% %download_url%%TBver%/tor-browser-%TBver%-android-armv7-multi.apk.asc
+%wget% %opt% %dist_url%%TBver%/tor-linux32-debug.tar.xz
+%wget% %opt% %dist_url%%TBver%/tor-linux32-debug.tar.xz.asc
+%wget% %opt% %dist_url%%TBver%/tor-linux64-debug.tar.xz
+%wget% %opt% %dist_url%%TBver%/tor-linux64-debug.tar.xz.asc
+::
+%wget% %opt% %dist_url%%TBver%/tor-win32-%Tver%.zip
+%wget% %opt% %dist_url%%TBver%/tor-win32-%Tver%.zip.asc
+%wget% %opt% %dist_url%%TBver%/tor-win64-%Tver%.zip
+%wget% %opt% %dist_url%%TBver%/tor-win64-%Tver%.zip.asc
+::
+%wget% %opt% %dist_url%%TBver%/torbrowser-install-%TBver%_%locale%.exe
+%wget% %opt% %dist_url%%TBver%/torbrowser-install-%TBver%_%locale%.exe.asc
+%wget% %opt% %dist_url%%TBver%/torbrowser-install-win64-%TBver%_%locale%.exe
+%wget% %opt% %dist_url%%TBver%/torbrowser-install-win64-%TBver%_%locale%.exe.asc
 ::
 echo download completed,have fun:-)
 echo .
